01f434a07fe080787a82bbabd6e1b6d75740f7b7
omerkarslioglu <omerkarsliogluu@gmail.com>
2024-12-12 22:11:09 +0300

    sobel out image added

--- src/matlab/image_show.m
+++ src/matlab/image_show.m
@@ -4,12 +4,18 @@ load lena.mat
 % Load output as hex file
 fid = fopen('lena_hex.txt', 'r');
 lena_hex = fscanf(fid, '%2x');
+
+fid0 = fopen('sobel_out.txt', 'r');
+sobel_out = fscanf(fid0, '%2x');
+
 fclose(fid);
+fclose(fid0);
 
 % Reshape and convert to uint8
 lena_hex = uint8(reshape(lena_hex, 256, 256));
-
+sobel_out = uint8(reshape(sobel_out, 256, 256));
 % Display input and output images side by side
 figure;
-subplot(1,2,1); imshow(lena); title('Input Image');
-subplot(1,2,2); imshow(lena_hex); title('Output Image');
+subplot(1,3,1); imshow(lena); title('Input Image');
+subplot(1,3,2); imshow(lena_hex); title('Hex Image');
+subplot(1,3,3); imshow(sobel_out); title('Sobel Out');
