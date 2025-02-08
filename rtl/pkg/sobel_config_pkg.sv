6d49afc8191b19a67b14e5d8f2103528955499b5
Ömer Karslıoğlu <67158049+omerkarslioglu@users.noreply.github.com>
2025-02-08 16:56:39 +0300

    upt. parametric file path

--- rtl/pkg/sobel_config_pkg.sv
+++ rtl/pkg/sobel_config_pkg.sv
@@ -2,6 +2,6 @@ package  sobel_config_pkg;
   localparam IMAGE_ROW_SIZE             = 256;
   localparam IMAGE_COLUMN_SIZE          = 256;
   localparam THRESHOLD                  = 100;
-  localparam string INPUT_FILE_PATH     = "C:/Users/omer/github_projects/sobel/src/picha.txt";
-  localparam string OUTPUT_FILE_PATH    = "C:/Users/omer/github_projects/sobel/src/matlab/sobel_out.txt";
+  localparam string INPUT_FILE_PATH     = "<input_file_path>;
+  localparam string OUTPUT_FILE_PATH    = "<output_file_path>";
 endpackage
